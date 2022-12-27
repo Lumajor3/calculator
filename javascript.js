e95d42d3aa57a1c30ee7256e229185d1446b3a27
Lumajor3 <ineedgold3@gmail.com>
2022-12-27 17:49:33 -0500

    Finish wiring of functions and update CSS
    -calculator follows order of operations
    -added styling
    
    TODO: don't let users put consecutive 0's or .'s

--- javascript.js
+++ javascript.js
@@ -1,37 +1,114 @@
 const calculatorScreen = document.querySelector(".calculator-screen");
+const clearButton = document.querySelector(".clear-button");
+const equalsButton = document.querySelector(".calculator-equals-key");
+const operatorButtons = document.querySelectorAll(".calculator-operator-key");
+const numberKeys = document.querySelectorAll(".calculator-number-key");
+
+let operatorPressed = false;
+let firstNum = null;
+let currentOperator = null;
+let firstKeyPressed = false;
 
 function add(num1, num2) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     return num1 + num2;
 }
 
 function subtract(num1, num2) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     return num1 - num2;
 }
 
 function multiply(num1, num2) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     return num1 * num2;
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    if (num2 == 0) {
+        clearCalculator();
+        alert("you knew better than to try that...")
+    } else {
+        return num1 / num2;
+    }
 }
 
 function operate(operator, num1, num2) {
     if (operator == "+") {
-        add(num1, num2);
+        return add(num1, num2);
     } else if (operator == "-") {
-        subtract(num1, num2);
-    } else if (operator == "*") {
-        multiply(num1, num2);
-    } else if (operator == "/") {
-        divide(num1, num2);
+        return subtract(num1, num2);
+    } else if (operator == "X") {
+        return multiply(num1, num2);
+    } else if (operator == "÷") {
+        return divide(num1, num2);
     }
 }
 
-const numberKeys = document.querySelectorAll(".calculator-number-key");
 numberKeys.forEach(key => {
     key.addEventListener("click", () => {
-        calculatorScreen.textContent = calculatorScreen.innerHTML + key.textContent;
+        updateCalculatorScreen(key.innerText);
     })
 })
 
+clearButton.addEventListener("click", () => clearCalculator());
+
+equalsButton.addEventListener("click", () => calculate(currentOperator))
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        makeOperation(button.innerText);
+    })
+})
+
+function makeOperation(operator) {
+    if (operatorPressed && firstNum != null) {
+        let calculationResult = operate(currentOperator, firstNum, calculatorScreen.innerText)
+        firstNum = calculationResult;
+        calculatorScreen.innerText = calculationResult;
+        currentOperator = operator;
+        firstKeyPressed = false;
+    } else if (!operatorPressed && firstNum == null) {
+        operatorPressed = true;
+        currentOperator = operator;
+        firstNum = calculatorScreen.innerText;
+        firstKeyPressed = false;
+    } else if (firstNum != null && !operatorPressed) {
+        currentOperator = operator;
+        operatorPressed = true;
+    }
+}
+
+function calculate() {
+    if (operatorPressed) {
+        let calculationResult = operate(currentOperator, firstNum, calculatorScreen.innerText)
+        calculatorScreen.innerText = calculationResult;
+        firstNum = calculationResult;
+        operatorPressed = false;
+        currentOperator = null;
+        firstKeyPressed = false;
+        console.log("working tree, your first number is: " + firstNum + " Working with an operator of: " + currentOperator)
+    }
+}
+
+function updateCalculatorScreen(numberKey) {
+    if (!firstKeyPressed) {
+        calculatorScreen.innerText = "" + numberKey
+        firstKeyPressed = true;
+    } else {
+        calculatorScreen.innerText = calculatorScreen.innerText + numberKey
+    }
+}
+
+function clearCalculator() {
+    calculatorScreen.innerText = 0;
+    currentOperator = null;
+    firstNum = null;
+    operatorPressed = false;
+    firstKeyPressed = false;
+}
